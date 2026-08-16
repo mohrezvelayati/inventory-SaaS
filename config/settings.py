65345a06e961649fa22fdb3cf9e3c8d52dad36d4
mohrezvelayati <mohammadrv2003@gmail.com>
2026-08-16 11:28:50 +0330

    feat: add global DRF pagination

--- config/settings.py
+++ config/settings.py
@@ -169,6 +169,9 @@ REST_FRAMEWORK = {
     ),
 
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 20,
 }
 
 # Swagger Configuration
